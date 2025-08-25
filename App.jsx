cd6f7853a58d590ac99118b11923940c2c7fa25b
John Oyekanmi <133876528+Zeusdml@users.noreply.github.com>
2025-08-25 02:10:21 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import About from './components/About'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import Header from './Header'
+import Hero from './Hero'
+import About from './About'
+import Projects from './Projects'
+import Skills from './Skills'
+import Contact from './Contact'
+import Footer from './Footer'
 
 function App() {
   return (
